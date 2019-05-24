f527acb79dc2a2522042fbddfa7b3293e2e1de2b
daisukekodaira <daisuke.kodaira03@gmail.com>
2019-05-24 20:01:38 +0900

    Release on 24th May

--- getDemandModel/DMget_main.m
+++
@@ -1,6 +0,0 @@
-clear all;
-clc;
-close all;
-y_pred = getDemandModel([pwd,'\','DST_201905191218.csv'],...
-                        [pwd,'\','DFP_201905191218.csv'],...
-                        [pwd,'\','ResultData.csv'])
--- setDemandModel/DMset_main.m
+++
@@ -1,4 +0,0 @@
-clear all;
-clc;
-close all;
-y_pred = setDemandModel([pwd, '\','DLT_201905191218.csv'])
