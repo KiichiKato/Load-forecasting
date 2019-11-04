180d27482a426f6cfdb049a91e924c5cb0624687
daisukekodaira <daisuke.kodaira03@gmail.com>
2019-11-04 20:05:28 +0900

    updated the code and test data

--- setDemandModel/DMset_setDemandModel.m
+++ setDemandModel/DMset_setDemandModel.m
@@ -8,7 +8,7 @@
 %         flag = -1; if operation fails.
 % ----------------------------------------------------------------------------
 
-function flag = setDemandModel(LongTermPastData)
+function flag = DMset_setDemandModel(LongTermPastData)
     tic;    
     
     %% Input errors check and Load data
@@ -51,28 +51,28 @@ function flag = setDemandModel(LongTermPastData)
     end
     %% Optimize the coefficients for the additive model
     coeff = DMset_pso_main(y_ValidEstIndv, valid_data(:,end)); 
-
+    % Get the number of individual forecasting algorithms (kmeans, ANN....)
+    n_algorithms = size(coeff(1).data,1);
+    
     %% Generate probability interval using validation result
+    % Arrange an empty matrix 'y_est' in advane
+    y_est = zeros(96, ValidDays);
+    % Get combined estimation value 'y_est' based on optimal coefficients and deterministic forecasting
     for hour = 1:24
-        for i = 1:size(coeff(1).data,1)
-            if i == 1
-                y_est(1+(hour-1)*4:hour*4,:) = coeff(hour).data(i).*y_ValidEstIndv(i).data(1+(hour-1)*4:hour*4,:);
-            else
+        for i = 1:n_algorithms
                 y_est(1+(hour-1)*4:hour*4,:) = y_est(1+(hour-1)*4:hour*4,:) + coeff(hour).data(i).*y_ValidEstIndv(i).data(1+(hour-1)*4:hour*4,:);  
-            end
         end
     end    
-    % Restructure
+    % Restructure the matrix format from "96*validation days" to one column 
     for day = 1:ValidDays        
         y_ValidEstComb(1+(day-1)*96:day*96, 1) = y_est(:, day);
-    end
-    
-    % error from validation data: error, hours, Quaters
+    end    
+    % Get error from validation data: absolute error, hours, Quaters
     err = [y_ValidEstComb - valid_data(:, end) valid_predictors(:,5) valid_predictors(:,6)]; 
     % Initialize the structure for error distribution
     % structure of err_distribution.data is as below:
-    % row=25hours(0~24 in "LongTermPastData"), columns=4quarters.
-    % For instance, "err_distribution(1,1).data" means 0am 0(first) quarter, which contains array like [e1,e2,e3....] 
+    %   row=25hours(0~24 in "LongTermPastData"), columns=4quarters.
+    %   For instance, "err_distribution(1,1).data" means 0am 0(first) quarter, which contains array like [e1,e2,e3....] 
     for hour = 1:24
         for quarter = 1:4
             err_distribution(hour,quarter).err(1) = NaN;            
@@ -100,28 +100,26 @@ function flag = setDemandModel(LongTermPastData)
         matname = fullfile(filepath, [name(i).string extention]);
         save(matname, varX(i).value);
     end        
+    
+    % If the process properly works, give back flag as 1  
+    flag = 1;    
+    toc;
 
 %     % for debugging --------------------------------------------------------------------- 
-%     for i = 1:2
+%     for i = 1:n_algorithms
 %         for day = 1:ValidDays
 %             predicted_load(i).data(1+(day-1)*96:day*96,:) = y_ValidEstIndv(i).data(:,day);
 %         end
 %     end
-%     
 %     MAPE(1) = mean(abs(y_ValidEstComb - valid_data(:,end))*100./valid_data(:,end)); % combined
 %     MAPE(2) = mean(abs(predicted_load(1).data - valid_data(:,end))*100./valid_data(:,end)); % k-means
 %     MAPE(3) = mean(abs(predicted_load(2).data - valid_data(:,end))*100./valid_data(:,end)); % fitnet
 %     disp(['MAPE of demand mean: ', num2str(MAPE(1)), '[%]'])
 %     disp(['MAPE of kmeans: ', num2str(MAPE(2)), '[%]'])
 %     disp(['MAPE of fitnet: ', num2str(MAPE(3)), '[%]'])
-%     demandGraph(1:size(y_ValidEstComb,1), y_ValidEstComb, valid_data(:,end), [], 'Combined for forecast data'); % Combined
-%     demandGraph(1:size(y_ValidEstComb,1), predicted_load(1).data, valid_data(:,end), [], 'k-means for forecast data'); % k-means
-%     demandGraph(1:size(y_ValidEstComb,1), predicted_load(2).data, valid_data(:,end), [], 'fitnet ANN for forecast data'); % NN       
-% 
+%     DMset_demandGraph(1:size(y_ValidEstComb,1), y_ValidEstComb, valid_data(:,end), [], 'Combined for forecast data'); % Combined
+%     DMset_demandGraph(1:size(y_ValidEstComb,1), predicted_load(1).data, valid_data(:,end), [], 'k-means for forecast data'); % k-means
+%     DMset_demandGraph(1:size(y_ValidEstComb,1), predicted_load(2).data, valid_data(:,end), [], 'fitnet ANN for forecast data'); % NN       
 %     % for debugging --------------------------------------------------------------------- 
-    
-    
-    
-    flag = 1;    
-    toc;
+
 end
