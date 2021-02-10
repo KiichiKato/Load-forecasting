af3d8fb64da7a9d5584e9d8dc4b0d0b13a01eccf
Daisuke Kodaira <43132698+daisukekodaira@users.noreply.github.com>
2021-02-10 15:01:45 +0900

    updates

--- setDemandModel/DMset_main.m
+++ setDemandModel/DMset_main.m
@@ -2,4 +2,4 @@ clear all;
 clc;
 close all;
 %DMset_setDemandModel(学習�?�用�?�るcsvファイル,学習�?�る日数)
-y_pred = DMset_setDemandModel([pwd, '\','DLT_20180801KEPRI.csv'],1)
+y_pred = DMset_setDemandModel([pwd, '\','LongTermPastData.csv'],1)
